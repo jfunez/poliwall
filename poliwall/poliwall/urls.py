8a5c0c91a2b228d798b0f5b2a42a1e3b386ff412
jfunez <juan.funez@gmail.com>
2013-04-15 23:19:49 -0300

    agrego url-s de rosetta y de media/static para el runserver

--- poliwall/poliwall/urls.py
+++ poliwall/poliwall/urls.py
@@ -1,5 +1,5 @@
 from django.conf.urls import patterns, include, url
-
+from django.conf import settings
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
@@ -11,3 +11,17 @@ urlpatterns = patterns(
     url(r'^redactor/', include('redactor.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
+
+
+if 'rosetta' in settings.INSTALLED_APPS:
+    urlpatterns += patterns(
+        '',
+        url(r'^rosetta/', include('rosetta.urls')),
+    )
+
+if settings.DEBUG:
+    urlpatterns += patterns(
+        'django.views.static',
+        url(r'^static/(?P<path>.*)$', 'serve', {'document_root': settings.STATIC_ROOT}),
+        url(r'^media/(?P<path>.*)$',  'serve', {'document_root': settings.MEDIA_ROOT}),
+    )
