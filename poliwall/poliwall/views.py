206090bb4e11a6b8d004ba626c30c8fefd6ad365
jfunez <juan.funez@gmail.com>
2013-04-20 03:46:19 -0300

    agrego primer acercamiento al home y primeras pantallas

--- poliwall/poliwall/views.py
+++ poliwall/poliwall/views.py
@@ -2,6 +2,7 @@
 from django.template import Context, RequestContext
 from django.shortcuts import render_to_response
 from lockdown.decorators import lockdown
+from polidata.models import Politician
 
 
 @lockdown(superusers_only=True)
@@ -9,3 +10,26 @@ def home(request):
     context = Context({
     })
     return render_to_response('home.html', context, context_instance=RequestContext(request))
+
+
+@lockdown(superusers_only=True)
+def gob_nacional(request):
+    context = Context({
+    })
+    return render_to_response('gob_nacional.html', context, context_instance=RequestContext(request))
+
+
+@lockdown(superusers_only=True)
+def poder_legislativo(request):
+    context = Context({
+    })
+    return render_to_response('poder_legislativo.html', context, context_instance=RequestContext(request))
+
+
+@lockdown(superusers_only=True)
+def politician_list(request):
+    politicians = Politician.objects.all()
+    context = Context({
+        'politicians': politicians,
+    })
+    return render_to_response('politician_list.html', context, context_instance=RequestContext(request))
