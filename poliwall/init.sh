3948e33f7941d9da59fdd2ca34f98d3d4fb9c22f
juanpex <jpma55@gmail.com>
2013-04-28 07:24:11 -0300

    correciones menores

--- poliwall/init.sh
+++ poliwall/init.sh
@@ -5,15 +5,13 @@ SUPERUSER_MAIL="admin@poliwall.com"
 pip install -r requirements.txt
 export PYTHONPATH=`pwd`:$PYTHONPATH
 export DJANGO_SETTINGS_MODULE=poliwall.settings
-
 dropdb $DB_NAME
 createdb -E UTF8 $DB_NAME
 python manage.py syncdb --migrate --noinput
 python manage.py loaddata scrapping/initial_legislative.json
 python manage.py createsuperuser --username=$SUPERUSER_NAME --email=$SUPERUSER_MAIL
 cd scrapping
-# scrapy crawl senators
-# scrapy crawl deputies
 scrapy crawl politician --nolog
+scrapy crawl politicianbiography --nolog
 cd ..
 ./viaticos.sh
