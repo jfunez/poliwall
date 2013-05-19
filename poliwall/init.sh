64521d85ee886d44d2afcf3feb79b34401e42f25
juanpex <jpma55@gmail.com>
2013-05-19 20:56:33 -0300

    se agrega instalador

--- poliwall/init.sh
+++ poliwall/init.sh
@@ -8,6 +8,7 @@ export DJANGO_SETTINGS_MODULE=poliwall.settings
 dropdb $DB_NAME
 createdb -E UTF8 $DB_NAME
 python manage.py syncdb --migrate --noinput
+python manage.py loaddata apps/polidata/fixrures/initial_user.json
 python manage.py loaddata scrapping/initial_legislative.json
 python manage.py createsuperuser --username=$SUPERUSER_NAME --email=$SUPERUSER_MAIL
 rm -rf media/polidata/politician/*
